2ce71a674660120d4cc006578ebffd9ccb75b302
nithin020 <88728943+nithin020@users.noreply.github.com>
2023-07-03 20:50:41 +0530

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,6 @@
+admin_credentials = "admin_database.csv"
+food_database = "food_database.csv"
+five_digit_low = 10000
+five_digit_high = 99999
+user_database = "user_database.csv"
+resources = "resources"
